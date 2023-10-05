fec5d9c72e2cd1cb27cd97bb4d9ad1b60ddeeff2
Sayan Chakraborty <104414720+sayan3456@users.noreply.github.com>
2023-10-05 11:17:23 +0530

    Update Header.js

--- src/containers/Layout/Header.js
+++ src/containers/Layout/Header.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 // style
 import './header.style.scss'
@@ -8,14 +9,38 @@ import Avatar from 'components/atoms/Avatar'
 
 function Header() {
   return (
-    <div className="header__container">
-      <div className="header__logo" />
-      <div className="header__right">
-        <div className="header__avatar">
-          <Avatar src="./assets/images/avatar.png" />
-        </div>
-      </div>
-    </div>
+   <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <a className="navbar-brand" href="#"> <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="">
+    Trello</a>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul className="navbar-nav mr-auto">
+      <li className="nav-item">
+        <a className="nav-link" href="#">Features</a>
+      </li>
+      <li className="nav-item>
+        <a className="nav-link" href="#">Solution</a>
+      </li>
+      <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Plan
+        </a>
+      </li>
+     <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Resource
+        </a>
+      </li>
+    </ul>
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
   )
 }
 
